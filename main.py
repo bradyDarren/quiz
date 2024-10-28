2d21e79f9cd39d3c7e3d867f9a99549a2aed5d26
bradyDarren <dcgbfinancial@gmail.com>
2024-10-28 10:05:29 -0500

    added for loop

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from data import question_bank
+from data import question_data
 from question_model import Question
 
+question_bank = []
+
+for question in question_data:
+    q_bank = Question(question["question"], question["answer"])
+    question_bank.append({f"Question: {q_bank.text} : Answer: {q_bank.answer}"})
+print(question_bank)
