8b6982c28c1c95a46dd39e08bba4bf7d2ad55ce3
bradyDarren <dcgbfinancial@gmail.com>
2024-10-30 18:03:30 -0500

    creation of the quizbrain file

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from data import question_data
 from question_model import Question
+from quizbrain import QuizBrain
 
 question_bank = []
 
 for question in question_data:
     q_bank = Question(question["question"], question["answer"])
     question_bank.append(q_bank)
-print(question_bank[1].text)
+
+check = QuizBrain(question_bank)
+
+check2 = check.get_question_number()
