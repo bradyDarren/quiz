a41170834a27b23967ce6636e626404b0a54f181
bradyDarren <dcgbfinancial@gmail.com>
2024-10-30 11:37:39 -0500

    removed the string values

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ question_bank = []
 
 for question in question_data:
     q_bank = Question(question["question"], question["answer"])
-    question_bank.append({f"Question: {q_bank.text} : Answer: {q_bank.answer}"})
-print(question_bank)
+    question_bank.append(q_bank)
+print(question_bank[1].text)
