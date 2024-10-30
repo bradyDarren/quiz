2fef823f3468e88b6c74b14c1ead32a94a50f64a
bradyDarren <dcgbfinancial@gmail.com>
2024-10-30 18:04:06 -0500

    creation of the get_question_number method

---
+++ quizbrain.py
@@ -0,0 +1,10 @@
+
+class QuizBrain:
+
+    def __init__(self, q_list) -> None:
+        self.question_number = 0
+        self.question_list = q_list
+
+    def get_question_number(self):
+        current_question = self.question_list[self.question_number]
+        user_answer = input(f"Q. #{self.question_number + 1} {current_question.text} (True/False)? ")
