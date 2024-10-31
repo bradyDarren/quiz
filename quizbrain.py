565aaecb94064f7076e817954d1680eb231285e2
bradyDarren <dcgbfinancial@gmail.com>
2024-10-31 07:07:51 -0500

    addition of the else statement

--- quizbrain.py
+++ quizbrain.py
@@ -4,6 +4,7 @@ class QuizBrain:
     def __init__(self, q_list) -> None:
         self.question_number = 0
         self.question_list = q_list
+        self.answers_correct = 0
 
     def remaining_questions(self):
         if self.question_number > len(self.question_list): 
@@ -14,10 +15,13 @@ class QuizBrain:
     def get_next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q. #{self.question_number}: {current_question.text} (True/False)? ")
-        correct = 0
+        user_answer = input(f"Q. #{self.question_number}: {current_question.text} (True/False)? ").lower().strip()
         if user_answer == current_question.answer:
-            correct += 1
-            print("You got it correct")
+            self.answers_correct += 1
+            print("You got it correct.")
             print(f"The correct answer was {current_question.answer}.")
-            print(f"Your current score is: {correct}/{self.question_number}.")
+            print(f"Your current score is: {self.answers_correct}/{self.question_number}.")
+        else: 
+            print("Sorry, you are incorrect.")
+            print(f"The correct answer was {current_question.answer}.")
+            print(f"Your current score is: {self.answers_correct}/{self.question_number}.")
