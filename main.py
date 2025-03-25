cdb46c6d50f1ca953ac4b7be8b20ae1ec02e0481
bradyDarren <dcgbfinancial@gmail.com>
2025-03-25 00:33:19 -0500

    addition of parameters

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from quizbrain import QuizBrain
 question_bank = []
 
 for question in question_data:
-    q_bank = Question(question["question"], question["correct_answer"])
+    q_bank = Question(question["question"], (question["correct_answer"]).lower())
+    print(q_bank.text, q_bank.answer)
     question_bank.append(q_bank)
 
 quiz = QuizBrain(question_bank)
