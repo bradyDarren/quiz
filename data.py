a37efe0f5f08df9a0a6315c84bc5276cb4913273
bradyDarren <dcgbfinancial@gmail.com>
2025-03-25 00:34:58 -0500

    additional params passed in

--- data.py
+++ data.py
@@ -1,10 +1,15 @@
 import requests
 
-response = requests.get(url='https://opentdb.com/api.php?amount=10&type=boolean')
+parameters = {
+    'amount':10,
+    'type':'boolean',
+}
+
+response = requests.get(url='https://opentdb.com/api.php' , params=parameters)
 response.raise_for_status()
 
-question_data = response.json()
-print(question_data)
+question_data = response.json()['results']
+# print(question_data)
 
 # question_data = [
 #     {"question": "Snail blood is blue.", "answer": "true"},
