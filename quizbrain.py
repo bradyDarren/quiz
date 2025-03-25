f75ac5217411a0a20ad159778295d28a487bf8d7
bradyDarren <dcgbfinancial@gmail.com>
2025-03-25 00:41:42 -0500

    addition of the html module allowing us to enescape characters within our question

--- quizbrain.py
+++ quizbrain.py
@@ -1,3 +1,4 @@
+import html
 
 class QuizBrain:
 
@@ -15,7 +16,8 @@ class QuizBrain:
     def get_next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q. #{self.question_number}: {current_question.text} (True/False)? ").lower().strip()
+        q_text = html.unescape(self.current_question.text)
+        user_answer = input(f"Q. #{self.question_number}: {q_text} (True/False)? ").lower().strip()
         self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer, correct_answer):
