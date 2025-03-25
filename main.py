cda7f4d0fb593d2eef7568b608ba901f8c41dbfe
bradyDarren <dcgbfinancial@gmail.com>
2025-03-25 02:03:12 -0500

    testing of the UI

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from data import question_data
 from question_model import Question
 from quizbrain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 
 for question in question_data:
     q_bank = Question(question["question"], (question["correct_answer"]).lower())
-    print(q_bank.text, q_bank.answer)
     question_bank.append(q_bank)
 
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface()
 
-while quiz.remaining_questions():
-    quiz.get_next_question()
+# while quiz.remaining_questions():
+#     quiz.get_next_question()
 quiz.final_score(quiz.answers_correct, quiz.question_number)
