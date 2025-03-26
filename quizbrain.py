f269a51f64b7f1b8ea38c53ec9b53b922c3a8d9e
bradyDarren <dcgbfinancial@gmail.com>
2025-03-26 18:44:04 -0500

    additional work on get_next_question method using a more OOP approach

--- quizbrain.py
+++ quizbrain.py
@@ -14,11 +14,12 @@ class QuizBrain:
             return True
 
     def get_next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
         q_text = html.unescape(self.current_question.text)
-        user_answer = input(f"Q. #{self.question_number}: {q_text} (True/False)? ").lower().strip()
-        self.check_answer(user_answer, current_question.answer)
+        return (f"Q. #{self.question_number}: {q_text} (True/False)?")
+        # user_answer = input(f"Q. #{self.question_number}: {q_text} (True/False)? ").lower().strip()
+        # self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer == correct_answer:
