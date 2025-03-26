971b82acab2e8de8fefa46472fc6d44c99871a90
bradyDarren <dcgbfinancial@gmail.com>
2025-03-26 18:44:32 -0500

    additional commit

--- quizbrain.py
+++ quizbrain.py
@@ -17,7 +17,7 @@ class QuizBrain:
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
         q_text = html.unescape(self.current_question.text)
-        return (f"Q. #{self.question_number}: {q_text} (True/False)?")
+        return f"Q. #{self.question_number}: {q_text} (True/False)?"
         # user_answer = input(f"Q. #{self.question_number}: {q_text} (True/False)? ").lower().strip()
         # self.check_answer(user_answer, current_question.answer)
 
